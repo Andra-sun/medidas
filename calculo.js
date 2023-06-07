ce55d8111970a09b943b152e66cdd7c127bb97d4
Andra-sun <111245270+Andra-sun@users.noreply.github.com>
2023-06-06 21:42:25 -0300

    Add files via upload

--- calculo.js
+++ calculo.js
@@ -5,27 +5,25 @@ var inputResultado = document.getElementById("resposta");
 var btnCalcular = document.getElementById("printar");
 
 btnCalcular.addEventListener("click", function () {
-
     var medida1 = selectMedida1.value;
     var medida2 = selectMedida2.value;
 
     var valor = parseFloat(inputValor.value);
 
     if (medida1 && medida2 && !isNaN(valor)) {
-
         var resultado;
 
         if (medida1 === "km" && medida2 === "hm") {
             resultado = valor * 10;
         } else if (medida1 === "km" && medida2 === "dam") {
             resultado = valor * 100;
-        }else if (medida1 === "km" && medida2 === "m") {
-            resultado = valor * 1000; 
-        }else if (medida1 === "km" && medida2 === "dm") {
+        } else if (medida1 === "km" && medida2 === "m") {
+            resultado = valor * 1000;
+        } else if (medida1 === "km" && medida2 === "dm") {
             resultado = valor * 10000;
         } else if (medida1 === "km" && medida2 === "cm") {
             resultado = valor * 100000;
-        }else if (medida1 === "km" && medida2 === "mm") {
+        } else if (medida1 === "km" && medida2 === "mm") {
             resultado = valor * 1000000;
         }
 
@@ -33,13 +31,13 @@ btnCalcular.addEventListener("click", function () {
             resultado = valor / 10;
         } else if (medida1 === "hm" && medida2 === "dam") {
             resultado = valor * 10;
-        }else if (medida1 === "hm" && medida2 === "m") {
-            resultado = valor * 100; 
-        }else if (medida1 === "hm" && medida2 === "dm") {
+        } else if (medida1 === "hm" && medida2 === "m") {
+            resultado = valor * 100;
+        } else if (medida1 === "hm" && medida2 === "dm") {
             resultado = valor * 1000;
         } else if (medida1 === "hm" && medida2 === "cm") {
             resultado = valor * 10000;
-        }else if (medida1 === "hm" && medida2 === "mm") {
+        } else if (medida1 === "hm" && medida2 === "mm") {
             resultado = valor * 100000;
         }
 
@@ -47,27 +45,27 @@ btnCalcular.addEventListener("click", function () {
             resultado = valor / 100;
         } else if (medida1 === "dam" && medida2 === "hm") {
             resultado = valor / 10;
-        }else if (medida1 === "dam" && medida2 === "m") {
-            resultado = valor * 10; 
-        }else if (medida1 === "dam" && medida2 === "dm") {
+        } else if (medida1 === "dam" && medida2 === "m") {
+            resultado = valor * 10;
+        } else if (medida1 === "dam" && medida2 === "dm") {
             resultado = valor * 100;
         } else if (medida1 === "dam" && medida2 === "cm") {
             resultado = valor * 1000;
-        }else if (medida1 === "dam" && medida2 === "mm") {
+        } else if (medida1 === "dam" && medida2 === "mm") {
             resultado = valor * 10000;
         }
-        
+
         if (medida1 === "m" && medida2 === "km") {
             resultado = valor / 1000;
         } else if (medida1 === "m" && medida2 === "hm") {
             resultado = valor / 100;
-        }else if (medida1 === "m" && medida2 === "dam") {
-            resultado = valor / 10; 
-        }else if (medida1 === "m" && medida2 === "dm") {
+        } else if (medida1 === "m" && medida2 === "dam") {
+            resultado = valor / 10;
+        } else if (medida1 === "m" && medida2 === "dm") {
             resultado = valor * 10;
         } else if (medida1 === "m" && medida2 === "cm") {
             resultado = valor * 100;
-        }else if (medida1 === "m" && medida2 === "mm") {
+        } else if (medida1 === "m" && medida2 === "mm") {
             resultado = valor * 1000;
         }
 
@@ -75,13 +73,13 @@ btnCalcular.addEventListener("click", function () {
             resultado = valor / 10000;
         } else if (medida1 === "dm" && medida2 === "hm") {
             resultado = valor / 1000;
-        }else if (medida1 === "dm" && medida2 === "dam") {
-            resultado = valor / 100; 
-        }else if (medida1 === "dm" && medida2 === "m") {
+        } else if (medida1 === "dm" && medida2 === "dam") {
+            resultado = valor / 100;
+        } else if (medida1 === "dm" && medida2 === "m") {
             resultado = valor / 10;
         } else if (medida1 === "dm" && medida2 === "cm") {
             resultado = valor * 10;
-        }else if (medida1 === "dm" && medida2 === "mm") {
+        } else if (medida1 === "dm" && medida2 === "mm") {
             resultado = valor * 100;
         }
 
@@ -89,13 +87,13 @@ btnCalcular.addEventListener("click", function () {
             resultado = valor / 100000;
         } else if (medida1 === "cm" && medida2 === "hm") {
             resultado = valor / 10000;
-        }else if (medida1 === "cm" && medida2 === "dam") {
-            resultado = valor / 1000; 
-        }else if (medida1 === "cm" && medida2 === "m") {
+        } else if (medida1 === "cm" && medida2 === "dam") {
+            resultado = valor / 1000;
+        } else if (medida1 === "cm" && medida2 === "m") {
             resultado = valor / 100;
         } else if (medida1 === "cm" && medida2 === "dm") {
             resultado = valor / 10;
-        }else if (medida1 === "cm" && medida2 === "mm") {
+        } else if (medida1 === "cm" && medida2 === "mm") {
             resultado = valor * 10;
         }
 
@@ -103,21 +101,49 @@ btnCalcular.addEventListener("click", function () {
             resultado = valor / 1000000;
         } else if (medida1 === "mm" && medida2 === "hm") {
             resultado = valor / 100000;
-        }else if (medida1 === "mm" && medida2 === "dam") {
-            resultado = valor / 10000; 
-        }else if (medida1 === "mm" && medida2 === "m") {
+        } else if (medida1 === "mm" && medida2 === "dam") {
+            resultado = valor / 10000;
+        } else if (medida1 === "mm" && medida2 === "m") {
             resultado = valor / 1000;
         } else if (medida1 === "mm" && medida2 === "dm") {
             resultado = valor / 100;
-        }else if (medida1 === "mm" && medida2 === "cm") {
+        } else if (medida1 === "mm" && medida2 === "cm") {
             resultado = valor / 100;
         }
 
         if (medida1 === medida2) {
             resultado = valor;
         }
-   
-        inputResultado.value = resultado;
+
+        switch (medida2) {
+            case "km":
+                inputResultado.value = `${resultado}km`;
+                break;
+
+            case "hm":
+                inputResultado.value = `${resultado}hm`;
+                break;
+
+            case "dam":
+                inputResultado.value = `${resultado}dam`;
+                break;
+
+            case "m":
+                inputResultado.value = `${resultado}m`;
+                break;
+
+            case "dm":
+                inputResultado.value = `${resultado}dm`;
+                break;
+
+            case "cm":
+                inputResultado.value = `${resultado}cm`;
+                break;
+
+            case "mm":
+                inputResultado.value = `${resultado}mm`;
+                break;
+        }
     } else {
         var mensagens = [
             "ヘ⁠（⁠。⁠□⁠°⁠）⁠ヘ",
@@ -127,22 +153,48 @@ btnCalcular.addEventListener("click", function () {
             "ERROR",
             "😰",
             "(o_o;)",
-            "(oᗜo;)"
+            "(oᗜo;)",
         ];
 
-        var mensagemAleatoria = mensagens[Math.floor(Math.random() * mensagens.length)];
-        
+        var mensagemAleatoria =
+            mensagens[Math.floor(Math.random() * mensagens.length)];
+
         inputResultado.value = mensagemAleatoria;
     }
+    if (inputResultado.value === ',') {
+        inputResultado.value = mensagemAleatoria;
+    }
+      
 });
 
 function validarInput(event) {
     var tecla = event.which || event.keyCode;
     var valor = String.fromCharCode(tecla);
     var padrao = /[0-9.]/;
-    
+
     if (!padrao.test(valor)) {
         event.preventDefault();
         return false;
     }
 }
+
+var campoEntrada = document.getElementById("vrli");
+var sumirMensagem = document.getElementById("printar");
+var mensagemAviso = document.getElementById("mensagemAviso");
+
+campoEntrada.addEventListener("focus", function () {
+    mensagemAviso.style.display = "block";
+});
+
+campoEntrada.addEventListener("input", function () {
+    if (validarInput(event)) {
+        mensagemAviso.style.display = "none";
+    }
+});
+
+sumirMensagem.addEventListener("click", function () {
+    mensagemAviso.style.display = "none";
+});
+
+function validarInput(event) {}
+
